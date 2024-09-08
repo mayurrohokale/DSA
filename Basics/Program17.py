e2b433d18d060b54b5d54c6c01d35fc3404c85c4
mayurrohokale <mayurrohokale12345@gmail.com>
2024-09-08 22:39:14 +0530

    Practicng basic questions

---
+++ Basics/Program17.py
@@ -0,0 +1,14 @@
+# Find the largest among 3
+
+No1 = 0
+print("Enter the First Number")
+Number1 = int(input(No1))
+No2=0
+print("Enter the Second Number")
+Number2 = int(input(No2))
+No3 = 0
+Number3 = int(input(No3))
+
+if(Number1> Number2 > Number3):
+    print(Number1,"Number 1 is greater")
+
